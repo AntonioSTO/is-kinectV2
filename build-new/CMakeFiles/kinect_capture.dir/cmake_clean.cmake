47aad98e4d7609d597ca13330b7b606abba4b754
AntonioSTO <antoniostoliveira20@gmail.com>
2025-04-09 09:34:37 -0300

    buffer implementation | threads

---
+++ build-new/CMakeFiles/kinect_capture.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kinect_capture.dir/kinect_capture.cpp.o"
+  "CMakeFiles/kinect_capture.dir/kinect_capture.cpp.o.d"
+  "kinect_capture"
+  "kinect_capture.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kinect_capture.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
